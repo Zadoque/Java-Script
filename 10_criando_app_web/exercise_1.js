32be07a916ea276c4cba39751fb3ec0af806f529
Zadoque <zadoquepiresdedeus@gmail.com>
2025-02-05 15:05:58 -0300

    Changes in calculator

--- 10_criando_app_web/exercise_1.js
+++ 10_criando_app_web/exercise_1.js
@@ -12,6 +12,9 @@ const addToDisplay = num => {
 
             num = ')';
         }
+        if((num === '/' || num === '*') && display.textContent[display.textContent.length - 1] == '('){
+            return;
+        }
         if( num === '.'){
             display.textContent += num;
         }
@@ -33,20 +36,33 @@ function getInfo(str, index){
         number2 : '',
         string: str,
     }
-    while(/[0-9\.]/g.test(str[info.index_start])){
-        info.number1 = `${str[info.index_start]}${info.number1}`
-        info.index_start --;
+    if(/[0-9\.]/g.test(str[info.index_start])){
+        while(/[0-9\.]/g.test(str[info.index_start])){
+            info.number1 = `${str[info.index_start]}${info.number1}`
+            info.index_start --;
+        }
+        info.index_start++;
     }
-    info.index_start++;
+    else{
+        console.log('something went wrong');
+        return;
+    }
+    
     
     while( /[0-9\.]/g.test(str[info.index_end])){
         info.number2 += `${str[info.index_end]}`;
         info.index_end++;
     }
     
-    if(info.index_start == 1 && str[0] === '-'){
+    if((info.index_start == 1 && str[0] === '-') || (str[info.index_start - 1] === '-' && str[info.index_start - 2] === '(' )){
         info.number1 = `${Number(info.number1) * (-1)}`
-        info.string = str.slice(1);
+        if(info.index_start === 1){
+            info.string = str.slice(1);
+        }
+        else{
+            info.string = `${str.slice(0,info.index_start - 1)}${str.slice(info.index_start)}`;
+            
+        }
         info.index_end--;
         info.index_start--;
     }
@@ -63,29 +79,58 @@ function calculate(str){
         if(str.includes('(')){
             let index_a = str.search(/[(]/g);
             let index_b = str.search(/[)]/g);
-            if(str.slice(index_a + 1).includes('(')){
-                while(str.slice(index_a + 1).includes('(')){
-                    index_a = str.slice(index_a + 1).search(/[(]/g) + 1;
+            let acc = 0;
+            let str1 = str;
+            if(str1.slice(index_a + 1).includes('(')){
+                while(str1.includes('(')){
+                    index_a = str1.search(/[(]/g);
+                    str1 = `${str1.slice(0, index_a)}${str1.slice(index_a + 1)}`;
+                    index_a += acc;
+                    acc++;
+                }
+                if(index_b < index_a){
+                    index_b = str.slice(index_b + 1).search(/[)]/g);
+                    console.log(str.slice(index_b + 1));
+                    index_b += index_a - 1;
                 }
             }
-            if(/[0-9]/g.test(str[index_a - 1])){
+
+            if(/[0-9)]/g.test(str[index_a - 1])){
                 str = `${str.slice(0, index_a)}*${str.slice(index_a)}`;
                 console.log('here');
                 index_a++;
                 index_b++;
             }
+            
             let result = `${calculate(str.slice(index_a + 1, index_b))}`
-            if(str[index_a - 1] === '-'){
+           
+            if(str[index_a - 1] === '-' && result !== '0'){
                 result = `${Number(result) * (-1)}`;
                 index_a--;
             }
             if(str[index_a - 1] === '+'){
                 index_a--;
             } 
-            
-            console.log(str[index_a - 1]);
+            if((str[index_a - 1] === '*' || str[index_a - 1] === '/' ) && Number(result) < 0){
+                let new_str = `${str.slice(0, index_a)}${result.slice(1)}`;
+                let info = getInfo(new_str, index_a - 1);
+                if(str[index_a - 1] === '*'){
+                    result = `${(Number(result) * Number(info.number1)).toFixed(2)}`;
+                }
+                else{
+                    if(result == '0'){
+                        return 'Dividir por zero é crime';
+                    }
+                    result = `${(Number(info.number1)  / Number(result) ).toFixed(2)}`;
+                }
+                str = `${info.string.slice(0, info.index_start)}${result}${str.slice(index_b + 1)}`
 
-            str = `${str.slice(0,index_a)}${result}${str.slice(index_b + 1)}`;
+            }
+            else{
+                str = `${str.slice(0,index_a)}${result}${str.slice(index_b + 1)}`;
+            }
+
+            
             console.log(str);
             
 
@@ -101,7 +146,10 @@ function calculate(str){
                 }
                 else{
                     let info = getInfo(str, index2);
-                    str = `${info.string.slice(0, info.index_start)}${(Number(info.number1) * Number(info.number2)).toFixed(2)}${info.string.slice(info.index_end, str.length - 1)}`;
+                    if(info.number2 == '0'){
+                        return 'Dividir por zero é crime';
+                    }
+                    str = `${info.string.slice(0, info.index_start)}${(Number(info.number1) / Number(info.number2)).toFixed(2)}${info.string.slice(info.index_end, str.length - 1)}`;
                     console.log(str);
                 }
             }
@@ -114,18 +162,21 @@ function calculate(str){
             else{
                 let index2 = str.search(/[\/]/g);
                 let info = getInfo(str, index2);
+                if(info.number2 == '0'){
+                    return 'Dividir por zero é crime';
+                }
                 str = `${(Number(info.number1) / Number(info.number2)).toFixed(2)}${info.string.slice(info.index_end, str.length - 1)}`;
                 console.log(str);
             }
             
         }
-        else if( (str.includes('+')) || (str.includes('-')) ){
+        else if( (str.slice(1).includes('+')) || (str.slice(1).includes('-')) ){
             if(str.includes('+') && str.includes('-')){
-                let index1 = str.slice(1).search(/[+]/g);
-                let index2 = str.slice(1).search(/[\-]/g);
+                let index1 = str.slice(1).search(/[+]/g) + 1;
+                let index2 = str.slice(1).search(/[\-]/g) + 1;
                 if (index1 > index2){
                     let info = getInfo(str, index1);
-                    str = `${info.string.slice(0, info.index_start)}${Number(info.number1) - Number(info.number2)}${info.string.slice(info.index_end + 1)}`;
+                    str = `${info.string.slice(0, info.index_start)}${Number(info.number1) + Number(info.number2)}${info.string.slice(info.index_end + 1)}`;
                     console.log(str);
                 }
                 else{
@@ -134,14 +185,14 @@ function calculate(str){
                     console.log(str);
                 }
             }
-            else if(str.includes('+')){
-                let index1 = str.search(/[+]/g);
+            else if(str.slice(1).includes('+')){
+                let index1 = str.slice(1).search(/[+]/g) + 1;
                 let info = getInfo(str, index1);
                 str = `${info.string.slice(0, info.index_start)}${Number(info.number1) + Number(info.number2)}${info.string.slice(info.index_end + 1)}`;
                 console.log(str);
             }
             else{
-                let index2 = str.search(/[\-]/g);
+                let index2 = str.slice(1).search(/[\-]/g) + 1;
                 let info = getInfo(str, index2);
                 str = `${Number(info.number1) - Number(info.number2)}${info.string.slice(info.index_end + 1)}`;
                 console.log(str);
@@ -149,7 +200,7 @@ function calculate(str){
         }
             
               
-    }while(/[\/+\-*]/g.test(str.slice(1)));
+    }while(/[\/+\-*)]/g.test(str.slice(1)));
     return str;
 }
 
@@ -197,12 +248,20 @@ document.querySelector('#buttons').addEventListener('click', event => {
                     display.textContent = calculate(str);
                 }
                 else{
-                    console.log('Nothing to do 1');
+                    let temp = display.textContent;
+                    display.textContent = 'Syntax Incorrect, try again'
+                    setTimeout(()=> {
+                        display.textContent = temp;
+                    },1500);
                 }
                 
             }
             else{
-                console.log('Nothing to do');
+                let temp = display.textContent;
+                    display.textContent = 'Syntax Incorrect, try again'
+                    setTimeout(()=> {
+                        display.textContent = temp;
+                    },1500);
             }
         }
     }
